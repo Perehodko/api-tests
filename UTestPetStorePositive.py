47a03d5b0d0e5f301ef9a2f06e23616382423489
Nadya <n.perehodko@yandex.com>
2019-02-26 13:37:18 +0300

    Add files via upload

--- UTestPetStorePositive.py
+++ UTestPetStorePositive.py
@@ -14,11 +14,9 @@ class TestPositiveTestsPet(unittest.TestCase):
                   "\n  \"name\": \"cat\",\n  \"photoUrls\": [\n    \"string\"\n  ],\n  \"tags\": " \
                   "[\n    {\n      \"id\": 0,\n      \"name\": \"string\"\n    }\n  ],\n  \"status\": " \
                   "\"available\"\n}\n\n"
-
         headers = {'Content-Type': "application/json"}
 
         response = requests.post(url, data=payload, headers=headers)
-        # print (response.text)
 
         self.assertEqual(200, response.status_code, \
                  ('Status code should be is 200. Current status code is %d.' % response.status_code))
@@ -52,15 +50,15 @@ class TestPositiveTestsPet(unittest.TestCase):
     def testUpdatePet(self):
 
         url = "http://petstore.swagger.io/v2/pet/"
-
         payload = "{\n  \"id\": 11,\n  \"category\": {\n    \"id\": 0,\n    \"name\": \"Tom\"\n  },"\
                   "\n  \"name\": \"cat\",\n  \"photoUrls\": [\n    \"string\"\n  ],\n  \"tags\": "\
                   "[\n    {\n      \"id\": 1,\n      \"name\": \"wild\"\n    }\n  ],\n  \"status\": "\
                   "\"available\"\n}\n\n"
-
         headers = {'Content-Type': "application/json"}
+
         response = requests.put(url, data=payload, headers=headers)
 
+        # parsing JSON response
         myJSONResponse = json.loads(response.text)
         
         self.assertEqual(200, response.status_code, ('Status code should be is 200. Current status code is %d.'
@@ -82,6 +80,7 @@ class TestPositiveTestsPet(unittest.TestCase):
 
         response = requests.get(url, data=payload, headers=headers, params=querystring)
 
+        # parsing JSON response
         myJSONResponse = json.loads(response.text)
 
         n = 0
@@ -89,6 +88,7 @@ class TestPositiveTestsPet(unittest.TestCase):
             if myJSONResponse[i]['id']==11:
                 n += 1
                 print (myJSONResponse[i])
+
         self.assertEqual(1, n, "No record in database!!!")
 
 
@@ -104,6 +104,7 @@ class TestPositiveTestsPet(unittest.TestCase):
 
         response = requests.post(url+petId, data=payload, headers=headers, params = querystring)
 
+        # parsing JSON response
         myJSONResponse = json.loads(response.text)
 
         self.assertEqual(200, response.status_code, ('Status code should be is 200. Current status code is %d.'
