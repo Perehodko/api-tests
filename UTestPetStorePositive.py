ffa25a4cf0e3793e6106f0054ea086c71c17bb80
Nadya <n.perehodko@yandex.com>
2019-02-28 14:13:06 +0300

    Add files via upload

--- UTestPetStorePositive.py
+++ UTestPetStorePositive.py
@@ -11,17 +11,34 @@ class TestPositiveTestsPet(unittest.TestCase):
     def testCreatePet(self):
 
         url = "http://petstore.swagger.io/v2/pet"
-        payload = "{\n  \"id\": 11,\n  \"category\": {\n    \"id\": 0,\n    \"name\": \"Tom\"\n  }," \
-                  "\n  \"name\": \"cat\",\n  \"photoUrls\": [\n    \"string\"\n  ],\n  \"tags\": " \
-                  "[\n    {\n      \"id\": 0,\n      \"name\": \"string\"\n    }\n  ],\n  \"status\": " \
-                  "\"available\"\n}\n\n"
-        headers = {'Content-Type': "application/json"}
+        payload = \
+            {
+                "id": 11,
+                "category": {
+                    "id": 0,
+                    "name": "Tom"
+                },
+                "name": "cat",
+                "photoUrls": [
+                    "string"
+                ],
+                "tags": [
+                    {
+                        "id": 0,
+                        "name": "string"
+                    }
+                ],
+                "status": "available"
+            }
 
-        response = requests.post(url, data=payload, headers=headers)
+        headers = {'Content-Type': "application/json"}
+        # serialize an object to a JSON
+        payloadJSON = json.dumps(payload)
+
+        response = requests.post(url, data=payloadJSON, headers=headers)
 
         self.assertEqual(200, response.status_code, ('Status code should be is 200. Current status code is %d.'
                                                      % response.status_code))
-
 
     # Find pet by ID
     # GET /pet/{petId}
@@ -29,9 +46,8 @@ class TestPositiveTestsPet(unittest.TestCase):
 
         url = "http://petstore.swagger.io/v2/pet/"
         id = "11"
-        payload = ""
         headers = {'Content-Type': "application/json"}
-        response = requests.get(url + id, data=payload, headers=headers)
+        response = requests.get(url + id, headers=headers)
 
         # parsing JSON response
         myJSONResponse = json.loads(response.text)
@@ -41,57 +57,75 @@ class TestPositiveTestsPet(unittest.TestCase):
         self.assertEqual(11, myJSONResponse['id'],
                          ('Field value "id" should be is 11. Current status code is %d.' % myJSONResponse['id']))
         self.assertEqual("Tom", myJSONResponse['category']['name'],
-                         ('Field value "name" should be is "Tom". Current value is %s.' % myJSONResponse['category']['name']))
-        self.assertEqual("available",  myJSONResponse['status'],
-                         ('Field value "status" should be is "available". Current value is %s.' % myJSONResponse['status']))
-
+                         ('Field value "name" should be is "Tom". Current value is %s.' % myJSONResponse['category'][
+                             'name']))
+        self.assertEqual("available", myJSONResponse['status'],
+                         ('Field value "status" should be is "available". Current value is %s.' % myJSONResponse[
+                             'status']))
 
     # Update an existing pet
     # PUT /pet
     def testUpdatePet(self):
 
         url = "http://petstore.swagger.io/v2/pet/"
-        payload = "{\n  \"id\": 11,\n  \"category\": {\n    \"id\": 0,\n    \"name\": \"Tom\"\n  },"\
-                  "\n  \"name\": \"cat\",\n  \"photoUrls\": [\n    \"string\"\n  ],\n  \"tags\": "\
-                  "[\n    {\n      \"id\": 1,\n      \"name\": \"wild\"\n    }\n  ],\n  \"status\": "\
-                  "\"available\"\n}\n\n"
-        headers = {'Content-Type': "application/json"}
+        payload = \
+            {
+                "id": 11,
+                "category": {
+                    "id": 0,
+                    "name": "Tom"
+                },
+                "name": "cat",
+                "photoUrls": [
+                    "string"
+                ],
+                "tags": [
+                    {
+                        "id": 1,
+                        "name": "wild"
+                    }
+                ],
+                "status": "available"
+            }
 
-        response = requests.put(url, data=payload, headers=headers)
+        headers = {'Content-Type': "application/json"}
+        # serialize an object to a JSON
+        payloadJSON = json.dumps(payload)
+
+        response = requests.put(url, data=payloadJSON, headers=headers)
 
         # parsing JSON response
         myJSONResponse = json.loads(response.text)
-        
+
         self.assertEqual(200, response.status_code,
                          ('Status code should be is 200. Current status code is %d.' % response.status_code))
         self.assertEqual(1, myJSONResponse['tags'][0]['id'],
-                         ('Field value "id" should be is 1. Current status code is %d.' % myJSONResponse['tags'][0]['id']))
+                         ('Field value "id" should be is 1. Current status code is %d.' % myJSONResponse['tags'][0][
+                             'id']))
         self.assertEqual("wild", myJSONResponse['tags'][0]['name'],
-                         ('Field value "name" should be is "wild". Current value is %s.' % myJSONResponse['tags'][0]['name']))
-
+                         ('Field value "name" should be is "wild". Current value is %s.' % myJSONResponse['tags'][0][
+                             'name']))
 
     # Finds Pets by status
     # GET /pet/findByStatus
     def testFindPetByStatusAvailable(self):
 
         url = "http://petstore.swagger.io/v2/pet/findByStatus"
-        querystring = {"status":"available"}
-        payload = ""
+        querystring = {"status": "available"}
         headers = {'Content-Type': "application/json"}
 
-        response = requests.get(url, data=payload, headers=headers, params=querystring)
+        response = requests.get(url, headers=headers, params=querystring)
 
         # parsing JSON response
         myJSONResponse = json.loads(response.text)
 
         n = 0
         for i in range(len(myJSONResponse)):
-            if myJSONResponse[i]['id']==11:
+            if myJSONResponse[i]['id'] == 11:
                 n += 1
                 print (myJSONResponse[i])
 
         self.assertEqual(1, n, "No record in database!!!")
-
 
     # Updates a pet in the store with form data
     # POST /pet/{petId}
@@ -100,10 +134,9 @@ class TestPositiveTestsPet(unittest.TestCase):
         url = "http://petstore.swagger.io/v2/pet/"
         petId = "11"
         querystring = {"status": "sold"}
-        payload = ""
         headers = {'Content-Type': "application/x-www-form-urlencoded"}
 
-        response = requests.post(url+petId, data=payload, headers=headers, params = querystring)
+        response = requests.post(url + petId, headers=headers, params=querystring)
 
         self.assertEqual(200, response.status_code,
                          ('Status code should be is 200. Current status code is %d.' % response.status_code))
@@ -111,6 +144,3 @@ class TestPositiveTestsPet(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-
-
-
